a3569c73347e092affa6b510c4eac2d8ebaaa889
zyluzr-ui <zyluzr@gmail.com>
2025-12-02 20:55:01 +0800

    Add carousel item preloading functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write 
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll("#ContemporaryCarousel .carousel-item");
+
+    items.forEach((item, index) => {
+        const total = items.length;
+
+        const leftIndex = (index - 1 + total) % total;
+        const rightIndex = (index + 1) % total;
+
+        const leftSrc = items[leftIndex].querySelector(".peek-center").src;
+        const rightSrc = items[rightIndex].querySelector(".peek-center").src;
+
+        item.querySelector(".peek-left").src = leftSrc;
+        item.querySelector(".peek-right").src = rightSrc;
+    });
+});
